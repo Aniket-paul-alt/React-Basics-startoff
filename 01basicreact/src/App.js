2156e7b5ac6194a964fa2ea94ac99fbfdca3196c
Aniket-paul-alt <113275960+Aniket-paul-alt@users.noreply.github.com>
2024-07-10 11:41:20 +0530

    Starting React journey

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,7 @@
+function App() {
+  return (
+    <h1>Chai aur React with create-react-app | Aniket Paul</h1>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,11 @@
+
+
+function App() {
+  
+
+  return (
+    <h1>Chai aur React with Vite | Aniket Paul</h1>
+  )
+}
+
+export default App
