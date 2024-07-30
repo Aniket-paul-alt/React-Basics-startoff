e1775f72d493580dd6b76a46056b73b2c3b08849
Aniket-paul-alt <113275960+Aniket-paul-alt@users.noreply.github.com>
2024-07-30 17:50:14 +0530

    done with contextApi and local storage

---
+++ 10todocontextLocal/src/contexts/index.js
@@ -0,0 +1,3 @@
+//This file for import everything to index.js and export all of them to whole project where its needed from index.js
+
+export {TodoContext, TodoProvider, useTodo} from "./TodoContext"
